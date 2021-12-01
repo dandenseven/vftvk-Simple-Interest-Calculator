df36c2c283ec2043eee90678b6dade38280d4f8e
dandenseven <31233404+dandenseven@users.noreply.github.com>
2021-12-01 14:51:20 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,17 +4,19 @@ function compute() {
     var rate = document.getElementsById("rate").value;
     var years = document.getElementById("years").value;
     var interest = principal * years * rate / 100;
-    var year = new Date().getFullYear() + parseInt(years);
+    var year = new Date().getFullYear()+parseInt(years);
     
+    //set to show once Compute interest  is clicked
+    document.getElementById("result").innerHTML = "If you deposit " +principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\</br>"
+
+
     // conditional statement to validate input box
-    if(principal <= 0){
+    if principal <= 0 => {
         alert("Enter a positive number");
         document.getElementById("principal").focus();
         return false;
     }
 
-    //set to show once Compute interest  is clicked
-    document.getElementById("result").innerHTML = "If you deposit " + principal + ",\<br\>at an interest rate of " + rate + "%\<br\>You will receive an amount of " + amount + ", \<br\>in the year " + year + "\</br>"
 
 }
 
