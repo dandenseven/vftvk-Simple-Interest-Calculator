21dcf6015c53eaf1af0b8565ef255f75fa471ef8
dandenseven <31233404+dandenseven@users.noreply.github.com>
2021-12-02 16:52:18 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
 function compute() {
     //variables created for inputs
-    var principal = document.getElementById("principal").value;
-    var rate = document.getElementsById("rate").value;
-    var years = document.getElementById("years").value;
+    let principal = document.getElementById("principal").value;
+    let rate = document.getElementsById("rate").value;
+    let years = document.getElementById("years").value;
     
-    var interest = principal * years * rate / 100;
-    var year = new Date().getFullYear() + parseInt(years);
+    let interest = principal * years * rate / 100;
+    let year = new Date().getFullYear() + parseInt(years);
     
-    
-    //set to show once Compute interest  is clicked
-//     var result = document.getElementById("result").innerHTML = "If you deposit " +principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\</br>"
 
 
     // conditional statement to validate input box
@@ -19,15 +16,19 @@ function compute() {
         return false;
     }
 
+    // set to show once Compute interest is clicked
     document.getElementById("result").innerHTML = "If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\</br>"
 
 }
 
 function updateRate() {
     //reads and displays the value of the range slider
-    var rateval = document.getElementById("rate").value;
-    document.getElementById("rate_val").innerText=rateval
+    let rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
     
 }
+        
+        
+
         
         
