b92d051b9213bf94fd6561cd9fd910a9c19abf09
dandenseven <31233404+dandenseven@users.noreply.github.com>
2021-12-02 14:42:25 -0500

    Update script.js

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ function compute() {
         return false;
     }
 
-    document.getElementById("result").innerHTML = "If you deposit <mark>${principal}</mark>,<br\>at an interest rate of <mark>${rate}%</mark>.<br\>You will receive an amount of <mark>${amount}</mark>,<br\>in the year <mark>${year}</mark></br>"
+    document.getElementById("result").innerHTML = "If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\</br>"
 
 }
 
