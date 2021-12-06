a1e674325d087b7c53072858c8209c66ba233cf6
dandenseven <31233404+dandenseven@users.noreply.github.com>
2021-12-06 16:13:27 -0500

    Update script.js

--- script.js
+++ script.js
@@ -16,11 +16,11 @@ function compute() {
     }
 
     // set to show once Compute interest is clicked
-    var name = "If you deposit "+<mark>principal</mark>+",<br\>at an interest rate of "+<mark>rate</mark>+"%<br\>You will receive an amount of "+<mark>interest</mark>+",\<br\>in the year "+<mark>year</mark>+"\<br\>"
+    var name = "If you deposit "+principal+",<br\>at an interest rate of "+rate+"%<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>"
     // var name= `If you deposit <mark>${principal}</mark>,<br\>at an interest rate of <mark>${rate}%</mark><br\>You will receive an amount of <mark>${interest}</mark>,\<br\>in the year <mark>${year}</mark></br>`;
     document.getElementById("result").innerHTML= name;
    
-    "If you deposit "+<mark>principal</mark>+",<br\>at an interest rate of "+<mark>rate</mark>+"%<br\>You will receive an amount of "+<mark>interest</mark>+",\<br\>in the year "+<mark>year</mark>+"\<br\>"
+   
 }
 
 //reads and displays the value of the range slider
@@ -30,4 +30,4 @@ function updateRate() {
     
 }
         
- 
+    
