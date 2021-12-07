c295971903effefeb1114b546c79b87c844d8006
dandenseven <31233404+dandenseven@users.noreply.github.com>
2021-12-06 23:29:19 -0500

    Update script.js

--- script.js
+++ script.js
@@ -14,14 +14,15 @@ function compute() {
     var years = document.getElementById("years").value;
     
     var interest = principal * years * rate / 100;
-    var dateYear = new Date();
-    var year = parseInt(dateYear.getFullYear()) + parseInt(years);
+    var year = new Date().getFullYear() + parseInt(years);
 
 
     // set to show once Compute interest is clicked
     var resultShow = document.getElementById("result");
-    // resultShow.innerHTML= "If you deposit " + "<span class='highlight'>" + principal + "</span>," + " <br\> at an interest rate of "+ "<span class='highlight'>" + rate + "</span>%" + "<br\>You will receive an amount of "+ "<span class='highlight'>" + interest + "</span>" + ",<br\> in the year " + "<span class='highlight'> " + year + "</span>";
-    resultShow.innerHTML= "if you deposit "+ <mark>${principal}</mark> + ",\<br\> at an interest rate of" + <mark>${rate}</mark>+"%\<br> You will receive an amount of " + <mark>${interest}</mark> + ",in the year" + <mark>${year}</mark>;
+    resultShow.innerHTML= "If you deposit " + "<span class='highlight'>" + principal + "</span>," + " <br\> at an interest rate of "+ "<span class='highlight'>" + rate + "</span>%" + "<br\>You will receive an amount of "+ "<span class='highlight'>" + interest + "</span>" + ",<br\> in the year " + "<span class='highlight'> " + year + "</span>";
+
+    
+   
 
 }
 
