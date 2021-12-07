b47145876df0d7746a7de6407d65512a29cf1096
dandenseven <31233404+dandenseven@users.noreply.github.com>
2021-12-06 23:23:40 -0500

    Update script.js

--- script.js
+++ script.js
@@ -20,8 +20,8 @@ function compute() {
 
     // set to show once Compute interest is clicked
     var resultShow = document.getElementById("result");
-    resultShow.innerHTML= "If you deposit " + "<span class='highlight'>" + principal + "</span>," + " <br\> at an interest rate of "+ "<span class='highlight'>" + rate + "</span>%" + "<br\>You will receive an amount of "+ "<span class='highlight'>" + interest + "</span>" + ",<br\> in the year " + "<span class='highlight'> " + year + "</span>";
-    
+    // resultShow.innerHTML= "If you deposit " + "<span class='highlight'>" + principal + "</span>," + " <br\> at an interest rate of "+ "<span class='highlight'>" + rate + "</span>%" + "<br\>You will receive an amount of "+ "<span class='highlight'>" + interest + "</span>" + ",<br\> in the year " + "<span class='highlight'> " + year + "</span>";
+    resultShow.innerHTML= "if you deposit "+ <mark>${principal}</mark> + ",\<br\> at an interest rate of" + <mark>${rate}</mark>+"%\<br> You will receive an amount of " + <mark>${interest}</mark> + ",in the year" + <mark>${year}</mark>;
 
 }
 
